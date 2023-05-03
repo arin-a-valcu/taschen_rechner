5bea14f6ab740b5339a4db797989f709c48464ac
arin-a-valcu <132481409+arin-a-valcu@users.noreply.github.com>
2023-05-03 19:00:35 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
-    
+input.onPinTouchEvent(TouchPin.P3, ButtonEvent.Down, function () {
+    loesung = b + a
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     b = b + 1
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
-    
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     b = b - 1
 })
+input.onPinTouchEvent(TouchPin.P1, ButtonEvent.Click, function () {
+    b = 0
+    b += a
+    if (input.buttonIsPressed(Button.A)) {
+        a = a - 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        a = a + 1
+    }
+})
+let a = 0
+let loesung = 0
 let b = 0
 b = 0
-basic.forever(function on_forever() {
+basic.forever(function () {
     basic.showNumber(b)
 })
