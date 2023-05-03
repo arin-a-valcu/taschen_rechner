96d7a9a70720b85cafc38f047c726139dc8f4d63
arin-a-valcu <132481409+arin-a-valcu@users.noreply.github.com>
2023-05-03 17:58:51 +0200

    Aktualisiere pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
+    
     b = b + 1
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
+    
     b = b - 1
 })
-let b = 0
+let b = 1
 b = 0
-basic.forever(function () {
+basic.forever(function on_forever() {
     basic.showNumber(b)
 })
